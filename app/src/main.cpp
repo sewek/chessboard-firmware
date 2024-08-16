c5ab50a20fb0bf7f02d365c21c5ce8390dd5c857
Seweryn Malczewski <seweryn.malczewski@gmail.com>
2024-08-16 20:36:47 +0200

    chore: Add test app file and configuration

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,17 +1,36 @@
+// #include <chess_position.h>
+#include <chess_position.h>
+#include <lvgl.h>
+#include <lvgl_display.h>
 #include <stdio.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/display.h>
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
 
 #define CONFIG_BOARD "nRF52840"
 
-int main()
-{
-   printk("Hello World! %s\n", CONFIG_BOARD);
+int main() {
+  printk("Hello World! %s\n", CONFIG_BOARD);
 
-   while (true)
-   {
-      k_msleep(1000);
-   }
+  ChessPosition position("a1");
+  position.isValid();
 
-   return 0;
+  // lv_disp_set_default(registered_displays[0]);
+
+  static lv_obj_t *meter;
+  meter = lv_meter_create(lv_scr_act());
+  lv_obj_center(meter);
+  lv_obj_set_size(meter, 200, 200);
+
+  lv_obj_remove_style(meter, nullptr, LV_PART_INDICATOR);
+  lv_task_handler();
+  // display_blanking_off(display_dev);
+
+  while (true) {
+    lv_task_handler();
+    k_msleep(1000);
+  }
+
+  return 0;
 }
