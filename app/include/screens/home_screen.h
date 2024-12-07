9d1479ba27c6cd2190a4deb1ccc1c199d87c826a
Dominika Lasa <lasa.dominika26@gmail.com>
2024-12-07 01:09:19 +0100

    chore: Add home screen

--- app/include/screens/home_screen.h
+++ app/include/screens/home_screen.h
@@ -8,6 +8,8 @@
 
 #include <base_screen.h>
 
+#include "images.h"
+
 /**
  * @brief This class represents the home screen of the application
  */
@@ -18,6 +20,17 @@ class HomeScreen : public BaseScreen {
 
   void init() override;
   void update() override;
+
+ private:
+  lv_obj_t* ui_Button1 = nullptr;
+  lv_obj_t* ui_Button2 = nullptr;
+  lv_obj_t* ui_Button3 = nullptr;
+  lv_obj_t* ui_Label1 = nullptr;
+  lv_obj_t* ui_Label2 = nullptr;
+  lv_obj_t* ui_Label3 = nullptr;
+  lv_obj_t* ui_Label4 = nullptr;
+  lv_obj_t* ui_Image1 = nullptr;
+  lv_obj_t* ui_Image6 = nullptr;
 };
 
 #endif  // HOME_SCREEN_H
